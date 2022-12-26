f0a7baa2a40b7846c2c1fbe0dbeb4555a36a48be
anhducbuui270802 <anhducbui272002@gmail.com>
2022-12-26 23:58:38 +0700

    lich su yeu thich

--- TimKiemPhongTro/Program.cs
+++ TimKiemPhongTro/Program.cs
@@ -16,7 +16,7 @@ namespace TimKiemPhongTro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrangChu());
+            Application.Run(new DangTinMoi());
         }
     }
 }
