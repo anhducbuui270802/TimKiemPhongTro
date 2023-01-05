b4ba0f8ae597a31c2fe3cc93ca98d8824b589b81
anhducbuui270802 <anhducbui272002@gmail.com>
2023-01-05 08:55:59 +0700

    lay danh sach phong tu csdl

--- TimKiemPhongTro/components/DanhSachTinDang.cs
+++ TimKiemPhongTro/components/DanhSachTinDang.cs
@@ -19,19 +19,6 @@ namespace TimKiemPhongTro.components
 
             InitializeComponent();
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    baidang bd = new baidang();
-            //    //bd.Title = "Bai dang thu " + i.ToString();
-            //    //bd.GiaPhong = "2";
-            //    //bd.DienTich = "20m2";
-            //    //bd.DiaChi = "KTX khu B, Phường Linh Trung, Thành Phố Thủ Đức";
-            //    //bd.SDT = "01628907601";
-            //    //bd.ImgNguoiDung = Image.FromFile("resource\\icon\\defaultuser.jpeg");
-            //    //bd.ImgPhong = Image.FromFile("resource\\icon\\image.png");
-            //    flowLayoutPanel1.Controls.Add(bd);
-            //}
-
             dt = sql.GetDataToTable("select * from BAIDANG");
             foreach (DataRow dr in dt.Rows)
             {
